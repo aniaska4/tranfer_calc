785b5db8ab3cf7cce776d8e3c0c82871d6235cb3
aniaska4 <aniaska4@wp.pl>
2019-06-10 21:12:51 +0200

    add new functionality get time in

--- app.js
+++ app.js
@@ -1,52 +1,66 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const input = document.getElementById('time');
-    const add = document.getElementById('add');
-    const subtract = document.getElementById('subtract');
-    input.value = new Date().toLocaleTimeString();
-    let currentTime = new Date().getTime();
-    let i = 0;
+document.addEventListener("DOMContentLoaded", function() {
+  // setting current time:
+  const input = document.getElementById("time");
+  const add = document.getElementById("add");
+  const subtract = document.getElementById("subtract");
+  input.value = new Date().toLocaleTimeString();
+  let currentTime = new Date().getTime();
+  let i = 0;
 
-    add.addEventListener('click', function () {
-        console.log(i);
-        i = i + 1000 * 60 * 5;
-        input.value = new Date(currentTime + i).toLocaleTimeString();
+  //add and subtract 5 minutes to current time:
+  add.addEventListener("click", function() {
+    i = i + 1000 * 60 * 5;
+    input.value = new Date(currentTime + i).toLocaleTimeString();
+  });
+
+  subtract.addEventListener("click", function() {
+    console.log(i);
+    i = i - 1000 * 60 * 5;
+    input.value = new Date(currentTime + i).toLocaleTimeString();
+  });
+
+  //event getting all information about bank's transfers:
+  document.querySelector(".item-12").addEventListener("click", function(e) {
+    e.preventDefault();
+
+    //hours of outgoing transfers:
+    const nameBankOut = document.querySelector("#from-bank").value;
+
+    // get name of bank from array:
+    const dataName = allDataDetails.map(function(e) {
+      return e.name;
     });
-    subtract.addEventListener('click', function () {
-        console.log(i);
-        i = i - 1000 * 60 * 5;
-        input.value = new Date(currentTime + i).toLocaleTimeString();
-    });
+    console.log(dataName); //all banks in array
 
-    document.querySelector(".item-12").addEventListener("click", function (e) {
-        e.preventDefault();
-        const nameBankOut = document.querySelector("#from-bank").value //nazwa banku wychodzącego
-        const nameBankIn = document.querySelector("#to-bank").value //nazwa banku przychodzącego
+    //extracting hours of specific banks (OUT):
+    const nameOfBank = allDataDetails.find(item => item.name === nameBankOut);
+    const timeOutOfBank = nameOfBank.timeOut; //tablica z godzinami
+    const finalTimeOut = timeOutOfBank.find(e => input.value <= e);
 
-        // pobieram nazwy banków z tablicy
-        const dataName = allDataDetails.map(function (e) {
-            return e.name;
-        })
-        console.log(dataName) //wszystkie banki
+    console.log(timeOutOfBank);
+    console.log(finalTimeOut);
 
-        //wyciągnięcie godzin konkretnych banków
-        const isTheBankNameExiste = dataName.filter(function (e) {
-            const nameOfBank = allDataDetails.find(item => item.name === nameBankOut);
-            const timeOutOfBank = nameOfBank.timeOut;
-            const firstTime = timeOutOfBank.filter( e => {
-                if (input.value <= e) {
-                   console.log("godzina jest mniejsza")
-                } else {
-                    console.log("godzina jest większa")
-                }
-                return e
-            })
-        });
+    //hours of incoming transfers:
+    const nameBankIn = document.querySelector("#to-bank").value;
+    console.log(nameBankIn);
 
-        function byLength(e1, e2) {
-            e1 = e1.length;
-            e2 = e2.length;
-            return e1 > e2 ? 1 : (e1 < e2 ? -1 : 0);
-        }
+    //extracting hours of specific banks (IN):
+    const nameofBankIn = allDataDetails.find(item => item.name === nameBankIn);
+    const timeInOfBank = nameofBankIn.timeIn; //tablica z godzinami
+    const finalTimeIn = timeInOfBank.find(e => finalTimeOut <= e);
+    console.log(nameofBankIn);
+    console.log(timeInOfBank);
+    console.log(finalTimeIn);
 
-    })
+    // //pobranie innerHTML z HTML
+    // const select = document.querySelector("#from-bank");
+    // const select2 = function(sel) {
+    //   const selectbankname = sel.options[sel.selectedIndex].text;
+    //   return selectbankname;
+    // };
+
+    $(".information").html(`<span>Przelew wykonano o :  ${input.value} </span>
+        </br> <span>Środki zostaną zaksięgowane w banku  o godzinie: ${finalTimeOut} </span> 
+        </br> <span>Środki pojawią się w banku: o godzinie: ${finalTimeIn}</span>`);
+  });
 });
